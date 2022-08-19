a3a2c6274912756018cc34724d62db71834f7433
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-08-19 09:47:41 -0400

    refactor: :recycle: finish classroom 3 of POO in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,60 @@
-function sum(num: number,) {
-    return num + 1;
+interface IAccount {
+  titular: string;
+  saldo?: number;
 }
 
+class Account {
+  private titular: string;
+  private saldo: number;
 
-interface ISumAge {
-    name: string,
-    yearnBorn: number,
-    profissional?: string,
+  constructor({ titular, saldo = 0 }: IAccount) {
+    (this.titular = titular),
+      (this.saldo = saldo),
+      console.log("Bem vindo ao DioBank!");
+  }
+
+  myValues = (): number => {
+    return this.saldo;
+  };
+
+  deposit = (depositValue: number): number => (this.saldo += depositValue);
+
+  retiredValue = (retiredValue: number): number => {
+    const value: number = this.saldo - retiredValue;
+    return value;
+  };
 }
 
-function sumAge({ name, yearnBorn, profissional
-}: ISumAge): string {
-    const age: number = 2022 - yearnBorn;
+const createAccount: Account = new Account({
+  titular: "Luiz",
+  saldo: 1000,
+});
 
-    if(profissional == ""){
-        return `Olá ${name} você tem ${age} anos.`;
+console.log(createAccount);
+console.log(createAccount.myValues());
+console.log(createAccount.deposit(5000));
+console.log(createAccount.retiredValue(500));
 
-    }
+// function sum(num: number,) {
+//     return num + 1;
+// }
 
-    return `Olá ${name} você tem ${age} anos e sua profissão é ${profissional}.`;
+// interface ISumAge {
+//     name: string,
+//     yearnBorn: number,
+//     profissional?: string,
+// }
 
+// function sumAge({ name, yearnBorn, profissional
+// }: ISumAge): string {
+//     const age: number = 2022 - yearnBorn;
 
-}
+//     if(profissional == ""){
+//         return `Olá ${name} você tem ${age} anos.`;
 
+//     }
 
-console.log(sumAge({ name: "Luiz", yearnBorn: 2002, profissional: ''}));
+//     return `Olá ${name} você tem ${age} anos e sua profissão é ${profissional}.`;
+// }
+
+// console.log(sumAge({ name: "Luiz", yearnBorn: 2002, profissional: ''}));
