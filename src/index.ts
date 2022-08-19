15ad6d668f3bfb44e51d5489bca581a6eb41700a
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-08-19 09:26:04 -0400

    refactor: :recycle: finish classroom 2 of types in typscript

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,26 @@ function sum(num: number,) {
     return num + 1;
 }
 
-console.log(sum(2));
+
+interface ISumAge {
+    name: string,
+    yearnBorn: number,
+    profissional?: string,
+}
+
+function sumAge({ name, yearnBorn, profissional
+}: ISumAge): string {
+    const age: number = 2022 - yearnBorn;
+
+    if(profissional == ""){
+        return `Olá ${name} você tem ${age} anos.`;
+
+    }
+
+    return `Olá ${name} você tem ${age} anos e sua profissão é ${profissional}.`;
+
+
+}
+
+
+console.log(sumAge({ name: "Luiz", yearnBorn: 2002, profissional: ''}));
